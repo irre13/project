0bcf11326cec936823aa1aef2e8877265873711d
Andrei <example@gmail.com>
2021-11-25 23:55:05 +0300

    1stLesson

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 "use strict";
 
-const isChecked = false;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
 
-const isClose = false;
-console.log(isChecked || !isClose);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?","");
+const b = +prompt("На сколько оцените его?","");
+const c = prompt("Один из последних просмотренных фильмов?","");
+const d = +prompt("На сколько оцените его?","");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
 
